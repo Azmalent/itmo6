9e1b0270490a19efcd745c3c934c2cc8539eb789
Azmalent <dmitrysjestov@niuitmo.ru>
2019-05-09 13:26:34 +0300

    Create compilers2 project

---
+++ compilers/compilers2/lib/compilers2.ex
@@ -0,0 +1,18 @@
+defmodule Compilers2 do
+  @moduledoc """
+  Documentation for Compilers2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Compilers2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ compilers/compilers2/test/compilers2_test.exs
@@ -0,0 +1,8 @@
+defmodule Compilers2Test do
+  use ExUnit.Case
+  doctest Compilers2
+
+  test "greets the world" do
+    assert Compilers2.hello() == :world
+  end
+end
